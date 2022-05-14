53f7d199c16b53491182f0051b1cc322d00f8c61
Luevateros <luevateros@ciencias.unam.mx>
2022-05-14 15:09:10 -0500

    Primer sprint completo, detalles en admin-service.yml

---
+++ invoice/src/main/java/com/invoice/configuration/client/ProductClient.java
@@ -0,0 +1,21 @@
+package com.invoice.configuration.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.invoice.api.dto.DtoProduct;
+
+/*
+ * Sprint 3 - Requerimiento 5
+ * Agregar método updateProductStock para actualizar el stock de productos
+ */
+
+@FeignClient(name = "product-service")
+public interface ProductClient {
+
+	@GetMapping("product/{gtin}")
+	public ResponseEntity<DtoProduct> getProduct(@PathVariable("gtin") String gtin);
+
+}
